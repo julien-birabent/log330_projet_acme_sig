d3884873ba8239900795df88fd494d9b57bf6f63
Pierre-Marc Thibault <Pierre-Marc Thibault>
2017-06-26 08:47:59 -0400

    Ajout de la page de login et de registration.

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name log330ProjetAcmeSigApp.controller:loginCtrl
+ * @description
+ * # loginCtrl
+ * Controller of the log330ProjetAcmeSigApp
+ */
+angular.module('log330ProjetAcmeSigApp')
+  .controller('loginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/registration.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name log330ProjetAcmeSigApp.controller:registrationCtrl
+ * @description
+ * # registrationCtrl
+ * Controller of the log330ProjetAcmeSigApp
+ */
+angular.module('log330ProjetAcmeSigApp')
+  .controller('registrationCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
