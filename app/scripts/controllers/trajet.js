66f38926d5edc7b668cb7045b561a635fe04b99b
Pierre-Marc Thibault <Pierre-Marc Thibault>
2017-07-15 20:39:35 -0400

    Écriture d'une ligne de trajet terminée.

--- app/scripts/controllers/trajet.js
+++ app/scripts/controllers/trajet.js
@@ -8,7 +8,7 @@
  * Controller of the log330ProjetAcmeSigApp
  */
 angular.module('log330ProjetAcmeSigApp')
-  .controller('trajetCtrl', function ($scope, $http, $location, session) {
+  .controller('trajetCtrl', function ($scope, $http, $location, session, $routeParams) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,4 +16,23 @@ angular.module('log330ProjetAcmeSigApp')
     ];
 
     session.isAuthentified();
+
+    $scope.points = "[[0,0],[1,1]]";
+
+    $scope.saveTrajet = function() {
+      var data = {
+        "trajetId": $routeParams.trajetId,
+        "points": $scope.points
+      };
+
+      $http.post("scripts/saveTrajet.php", data)
+        .then(function(response) {
+          if (response.data != "false") {
+            alert("Le trajet a été sauvegardé");
+          } else {
+            $scope.error = "Une erreur s'est produite lors de la sauvegarde du trajet!";
+            $scope.fieldsError = true;
+          }
+        });
+    };
   });
